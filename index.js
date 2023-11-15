2a6a982d75c5285bdc7560a5b88c11661c411159
Abaniwonnda <oladimeji.abaniwonnda@pau.edu.ng>
2023-11-15 21:16:49 +0100

    ()

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ let attempt = document.querySelector("#attempts");
 let restart = document.querySelector('#restart')
 let attemptcount = 0;
 submit.addEventListener('click', () =>{
-    attemptcount++;
-    attempt.innerText ="Attempt Number: " + attemptcount.toString();
     let guess = parseInt(input.value, 10)
-    if (guess < randomNumber){
-        text.innerText = 'oops, too low!';
-        text.style.color = 'blue';
-    }
-    else if(isNaN(guess)){
+    if(isNaN(guess)){
         text.innerText = 'INVALID INPUT';
         text.style.color = 'red';
     }
+    else{
+    attemptcount++;
+    attempt.innerText ="Attempt Number: " + attemptcount.toString();
+    
+    if (guess < randomNumber){
+        text.innerText = 'oops, too low!';
+        text.style.color = 'blue';
+    } 
+
     else if(guess > randomNumber){
         text.innerText = 'oops, too high!';
         text.style.color = 'blue';
@@ -25,9 +28,9 @@ submit.addEventListener('click', () =>{
     }
     else{
         text.innerText = "THAT'S IT!!!";
-        text.style.color = 'green';
+        text.style.color = '#00FF00';
     }
-    
+}
 } )
 restart.addEventListener('click', () =>{
     randomNumber = Math.ceil(Math.random() * 10);
